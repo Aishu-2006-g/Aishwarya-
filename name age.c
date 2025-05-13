60cb9ac47a61471304dcabe59a787df892cbe0ae
Aishu-2006-g <aishumallari192006@gmail.com>
2025-05-13 15:41:07 +0530

    Create name age.c

---
+++ name age.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int name[50];
+    int  age[40];
+    int  bod[100];
+    int college [50];
+    int no[234];{
+   
+   printf("enter the name ");
+   scanf("%d",&name);
+   printf("enter the age ");
+   scanf("%d",&age);
+   printf("enter the bod");
+   scanf("%d",&bod);
+   printf("enter the college");
+   scanf("%d",&college);
+   printf("enter the no");
+   scanf("%d",&no);
+   
+   
+   return 0;
+    }
+   
+}
