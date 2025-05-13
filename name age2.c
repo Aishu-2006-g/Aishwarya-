8c860b35542e71e5cd05cfbab3defaf0d8cd48a8
Aishu-2006-g <aishumallari192006@gmail.com>
2025-05-13 15:48:46 +0530

    Create name age2.c

---
+++ name age2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int shridevi()
+ int main()
+ 
+ {
+    printf("aishwarya\n");
+    printf("19\n");
+    printf("123\n");
+    printf("shridevi\n");
+    printf("123456\n");
+    
+}
+int main()
+ {
+     shridevi();
+    return 0;
+}
