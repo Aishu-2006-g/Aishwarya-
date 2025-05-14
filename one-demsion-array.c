8228b6fc8e422b0ee555f92936ba4f06ad1d2c56
AISHWARYA <aishumallari192006@gmail.com>
2025-05-14 11:57:39 +0530

    Create one-demsion-array.c

---
+++ one-demsion-array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int row,column;
+   int aishu[8]={1,2,3,4,5,6,5,7};
+       
+  int main()
+  {
+       for(int i=0;i<2;i++){
+         for(int j=0;j<3;j++){
+             printf("%d\n",aishu[i]);
+             }
+       }
+   }
