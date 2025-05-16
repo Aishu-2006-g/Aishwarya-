ea2a9c881c78fc0710c623aed33ad9096ddba814
AISHWARYA <aishumallari192006@gmail.com>
2025-05-16 15:23:56 +0530

    Create stucu-num.c

---
+++ stucu-num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct aishwarya{
+        char name[50];
+        int roll;
+        float marks;
+    };
+    int main(){  
+    struct aishwarya s1;
+    printf("enter the name:");
+    scanf("%s"&s1,name);
+    printf("enter the roll:");
+    scanf("%d"&s1.roll);
+    printf("enter the marks:");
+    scanf("%f"&s1.marks);
+    printf("\n aishwarya details:\n");
+    printf("name:%s\nroll:%d\nmarks:%f\n/3,s1,roll,s1marks");
+  }
+return 0;
+}
