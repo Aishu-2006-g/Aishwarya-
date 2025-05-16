7815d2f4b827ac91cbf3b9b3d429d6475a6a68d0
AISHWARYA <aishumallari192006@gmail.com>
2025-05-16 10:55:16 +0530

    Create prime-num.c

---
+++ prime-num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int num,i,isprime=1;
+      printf("enter the num is positive number ");
+      scanf("%d",&num);
+     if(num<=1);
+     isprime=0;
+          for(i=2;i<=num/2;i++)
+          if(num%i==0);
+     if( 
+     printf("is a prime num\n"));
+     
+     else{
+         printf("is a not prime num\n");
+     }
+     return 0;
+     
+}
